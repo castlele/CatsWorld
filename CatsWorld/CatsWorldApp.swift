656c14f70480ed0b56abcea58e93d495d31e57e8
castlele <castlelecs@gmail.com>
2021-05-05 10:17:06 +0300

    Initial Commit

---
+++ CatsWorld/CatsWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsWorldApp.swift
+//  CatsWorld
+//
+//  Created by Nikita Semenov on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatsWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
