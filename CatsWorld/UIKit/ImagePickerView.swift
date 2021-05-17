13c400c3dff941c0616963431624e6ee770e5ae5
castlele <castlelecs@gmail.com>
2021-05-17 13:49:24 +0300

    ImagePicker added

---
+++ CatsWorld/UIKit/ImagePickerView.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePickerView.swift
+//  CatsWorld
+//
+//  Created by Nikita Semenov on 17.05.2021.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+	
+	final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+		let parent: ImagePicker
+		
+		init(_ parent: ImagePicker) {
+			self.parent = parent
+		}
+		
+		func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+			if let uiImage = info[.originalImage] as? UIImage {
+				parent.image = uiImage
+			}
+			
+			parent.presentation.wrappedValue.dismiss()
+		}
+	}
+	
+	@Environment(\.presentationMode) var presentation
+	@Binding var image: UIImage?
+	
+	func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+		let picker = UIImagePickerController()
+		picker.delegate = context.coordinator
+		return picker
+	}
+	
+	func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+		<#code#>
+	}
+	
+	func makeCoordinator() -> Coordinator {
+		Coordinator(self)
+	}
+}
